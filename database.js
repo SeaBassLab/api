329a2a27b7edb70c080a42a34a692a3097c9a02c
SeaBassLab <70863556+SeaBassLab@users.noreply.github.com>
2021-10-11 11:52:01 -0300

    trying

--- database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose"
-
-(async () => {
-    const db = await mongoose.connect("mongodb://Localhost/proyectar", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log('Database is connected to:', db.connection.name);
-})();
